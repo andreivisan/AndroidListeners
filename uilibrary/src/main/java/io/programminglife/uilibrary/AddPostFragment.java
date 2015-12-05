ea252d08d4345de17e0cb01e12b6591b3f1dbbea
Andrei Visan <andreivisan@andreis-mbp.home>
2015-12-05 15:41:36 +0100

    First commit

---
+++ uilibrary/src/main/java/io/programminglife/uilibrary/AddPostFragment.java
@@ -0,0 +1,7 @@
+package io.programminglife.uilibrary;
+
+/**
+ * Created by andreivisan on 12/5/15.
+ */
+public class AddPostFragment {
+}
---
+++ uilibrary/src/main/java/io/programminglife/uilibrary/listener/EditTextImeBackListener.java
@@ -0,0 +1,12 @@
+package io.programminglife.uilibrary.listener;
+
+import io.programminglife.uilibrary.customcomponents.CustomEditText;
+
+/**
+ * Created by andreivisan on 12/5/15.
+ */
+public interface EditTextImeBackListener {
+
+    void onImeBack(CustomEditText ctrl, String text);
+
+}
