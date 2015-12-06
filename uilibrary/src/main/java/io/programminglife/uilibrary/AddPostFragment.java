7c7e715d7243abab39e865d367ca0c90f6c83d1e
Andrei Visan <andrei.m.visan@gmail.com>
2015-12-06 20:51:43 +0100

    Added post view

--- uilibrary/src/main/java/io/programminglife/uilibrary/AddPostFragment.java
+++ uilibrary/src/main/java/io/programminglife/uilibrary/AddPostFragment.java
@@ -1,14 +1,17 @@
 package io.programminglife.uilibrary;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.RelativeLayout;
 
 import io.programminglife.uilibrary.customcomponents.CustomEditText;
+import io.programminglife.uilibrary.listener.EditTextImeBackListener;
 import io.programminglife.uilibrary.listener.PublishPostListener;
 
 /**
@@ -19,22 +22,44 @@ public class AddPostFragment extends Fragment {
     private CustomEditText mPostContent;
     private RelativeLayout mPostButton;
     private View mFragmentView;
+    private Fragment mCurrentFragment;
+    private InputMethodManager inputMethodManager;
 
     private PublishPostListener mPublishPostListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mFragmentView = inflater.inflate(R.layout.post_form, container, false);
+        mCurrentFragment = this;
 
         mPostContent = (CustomEditText)mFragmentView.findViewById(R.id.post_content);
         mPostButton = (RelativeLayout)mFragmentView.findViewById(R.id.post_button);
 
+        showInputKeyboard();
         initView();
 
         return mFragmentView;
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        inputMethodManager.hideSoftInputFromWindow(getView().getWindowToken(), 0);
+    }
+
+    private void showInputKeyboard() {
+        inputMethodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
     private void initView() {
+        mPostContent.setOnEditTextImeBackListener(new EditTextImeBackListener() {
+            @Override
+            public void onImeBack(CustomEditText ctrl, String text) {
+                getActivity().getSupportFragmentManager().beginTransaction().remove(mCurrentFragment).commit();
+            }
+        });
+
         mPostButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
