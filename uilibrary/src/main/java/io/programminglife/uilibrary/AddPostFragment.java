7baca6400e65d6afacf728bacd67b71a0faaf64d
Andrei Visan <andrei.m.visan@gmail.com>
2015-12-06 19:38:44 +0100

    Added post view

--- uilibrary/src/main/java/io/programminglife/uilibrary/AddPostFragment.java
+++ uilibrary/src/main/java/io/programminglife/uilibrary/AddPostFragment.java
@@ -1,7 +1,28 @@
 package io.programminglife.uilibrary;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
 /**
  * Created by andreivisan on 12/5/15.
  */
-public class AddPostFragment {
+public class AddPostFragment extends Fragment {
+
+    private EditText mPostContent;
+    private RelativeLayout mPostButton;
+    private View mFragmentView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mFragmentView = inflater.inflate(R.layout.post_form, container, false);
+
+        return mFragmentView;
+    }
 }
