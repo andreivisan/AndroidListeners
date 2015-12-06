98f37a8f325251a1d0857c47999307c33a2eedd5
Andrei Visan <andrei.m.visan@gmail.com>
2015-12-06 19:54:01 +0100

    Added post view

--- uilibrary/src/main/java/io/programminglife/uilibrary/AddPostFragment.java
+++ uilibrary/src/main/java/io/programminglife/uilibrary/AddPostFragment.java
@@ -1,7 +1,6 @@
 package io.programminglife.uilibrary;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,19 +8,23 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
+import io.programminglife.uilibrary.customcomponents.CustomEditText;
+
 /**
  * Created by andreivisan on 12/5/15.
  */
 public class AddPostFragment extends Fragment {
 
-    private EditText mPostContent;
+    private CustomEditText mPostContent;
     private RelativeLayout mPostButton;
     private View mFragmentView;
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mFragmentView = inflater.inflate(R.layout.post_form, container, false);
+
+        mPostContent = (CustomEditText)mFragmentView.findViewById(R.id.post_content);
+        mPostButton = (RelativeLayout)mFragmentView.findViewById(R.id.post_button);
 
         return mFragmentView;
     }
--- uilibrary/src/main/java/io/programminglife/uilibrary/customcomponents/CustomEditText.java
+++ uilibrary/src/main/java/io/programminglife/uilibrary/customcomponents/CustomEditText.java
@@ -1,7 +1,46 @@
 package io.programminglife.uilibrary.customcomponents;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.widget.EditText;
+
+import io.programminglife.uilibrary.listener.EditTextImeBackListener;
+
 /**
  * Created by andreivisan on 12/5/15.
  */
-public class CustomEditText {
+public class CustomEditText extends EditText {
+
+    private EditTextImeBackListener mOnImeBack;
+
+    public CustomEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public CustomEditText(Context context) {
+        super(context);
+
+    }
+
+    @Override
+    public boolean onKeyPreIme(int keyCode, KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+            if (mOnImeBack != null) {
+                mOnImeBack.onImeBack(this, this.getText().toString());
+            }
+        }
+        return super.dispatchKeyEvent(event);
+    }
+
+    public void setOnEditTextImeBackListener(EditTextImeBackListener listener) {
+        mOnImeBack = listener;
+    }
+
 }
