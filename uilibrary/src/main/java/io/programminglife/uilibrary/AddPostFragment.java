c2b9f4fdcefd12dbdb257dde983208797b9a942b
Andrei Visan <andrei.m.visan@gmail.com>
2015-12-06 20:05:18 +0100

    Added post view

--- uilibrary/src/main/java/io/programminglife/uilibrary/AddPostFragment.java
+++ uilibrary/src/main/java/io/programminglife/uilibrary/AddPostFragment.java
@@ -2,12 +2,14 @@ package io.programminglife.uilibrary;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import io.programminglife.uilibrary.customcomponents.CustomEditText;
+import io.programminglife.uilibrary.listener.PublishPostListener;
 
 /**
  * Created by andreivisan on 12/5/15.
@@ -18,6 +20,8 @@ public class AddPostFragment extends Fragment {
     private RelativeLayout mPostButton;
     private View mFragmentView;
 
+    private PublishPostListener mPublishPostListener;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mFragmentView = inflater.inflate(R.layout.post_form, container, false);
@@ -25,10 +29,23 @@ public class AddPostFragment extends Fragment {
         mPostContent = (CustomEditText)mFragmentView.findViewById(R.id.post_content);
         mPostButton = (RelativeLayout)mFragmentView.findViewById(R.id.post_button);
 
+        initView();
+
         return mFragmentView;
     }
 
     private void initView() {
+        mPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!TextUtils.isEmpty(mPostContent.getText())) {
+                    mPublishPostListener.onPostPublished(mPostContent.getText().toString());
+                }
+            }
+        });
+    }
 
+    public void setmPublishPostListener(PublishPostListener mPublishPostListener) {
+        this.mPublishPostListener = mPublishPostListener;
     }
 }
